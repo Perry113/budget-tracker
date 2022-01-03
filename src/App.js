270a294ffedb421e5b123b74d080738e841a4619
Perry113 <mm6mm@wp.pl>
2022-01-03 22:22:03 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Budget from './component/Budget';
+import Remaining from './component/Remaining';
+import ExpenseTotal from './component/ExpenseTotal';
+import ExpenseList from './component/ExpenseList';
+import AddExpenseForm from './component/AddExpenseForm';
+import { AppProvider } from './context/AppContext';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <AppProvider>
+        <div className='container'>
+            <h1 className='mt-3'> My Budget Planner </h1>
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                         <Budget/>
+                     </div>  
+                     <div className='col-sm'>
+                         <Remaining />
+                     </div>
+                     <div className='col-sm'>
+                         <ExpenseTotal />
+                     </div>
+                 </div> 
+                 <h3 className='mt-3'>Expenses</h3>
+                 <div className='row mt-3'>
+                     <div className='col-sm'>
+                        <ExpenseList />
+                     </div>
+                 </div>
+                 <h3 className='mt-3'>Add Expense</h3>
+                 <div className='mt-3'>
+                     <div className='col-sm'>
+                         <AddExpenseForm />
+                     </div>
+                 </div>
+        </div>
+
+        </AppProvider>
+
+    );
+};
 
 export default App;
+
+
+// column helping with responsing layout = div className='row mt-m-3'> 
+  //responsive laypout = div className='col-sm
